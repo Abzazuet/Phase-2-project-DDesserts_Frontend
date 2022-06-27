b9a27f0328affdfcffc63046793789bb2b76edff
Abel Zazueta <abel.zazueta@gmail.com>
2022-06-26 22:54:23 -0700

    Titlte changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
 
 import TopNavbar from "./TopNavbar";
 import './App.css';
+import { useEffect } from "react";
 
 function App() {
+  useEffect(()=>{
+    document.title = "Daniela's Desserts";
+  }, [])
   return (
     <div className="App">
       <TopNavbar />
--- src/components/Desserts.js
+++ src/components/Desserts.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import Dessert from "./Dessert";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 function Desserts() {
     const [desserts, setDesserts] = useState([]);
