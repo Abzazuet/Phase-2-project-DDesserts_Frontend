c17733c7ae3e38af060a848029ce86cfba654762
Abel Zazueta <abel.zazueta@gmail.com>
2022-06-27 18:59:01 -0700

    Fetch changed to app instead of it being in the dessert component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,23 +6,35 @@ import Login from "./Login";
 
 import AboutUs from "./AboutUs";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../styles/App.css";
 
 function App() {
   const [adminLogged, setAdminLogged] = useState(false);
+  const [desserts, setDesserts] = useState([]);
+  useEffect(() => {
+    fetch("https://immense-garden-31850.herokuapp.com/desserts")
+      .then((res) => res.json())
+      .then((data) => setDesserts(data));
+  }, []);
   return (
     <div className="App">
       <BrowserRouter>
-        <TopNavbar adminLogged={adminLogged} setAdminLogged={setAdminLogged}/>
+        <TopNavbar adminLogged={adminLogged} setAdminLogged={setAdminLogged} />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/aboutUs" element={<AboutUs />} />
-          <Route exact path="/desserts" element={<Desserts />} />
+          <Route
+            exact
+            path="/desserts"
+            element={<Desserts desserts={desserts} />}
+          />
           <Route
             exact
             path="/login"
-            element={<Login setLoggedIn={setAdminLogged} adminLogged={adminLogged}/>}
+            element={
+              <Login setLoggedIn={setAdminLogged} adminLogged={adminLogged} />
+            }
           />
           <Route exact path="/addDessert" element={<AddDessert />} />
         </Routes>
--- src/components/Desserts.jsx
+++ src/components/Desserts.jsx
@@ -1,15 +1,9 @@
-import { useEffect, useState } from "react";
 import Dessert from "./Dessert";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 
-function Desserts() {
-  const [desserts, setDesserts] = useState([]);
-  useEffect(() => {
-    fetch("https://immense-garden-31850.herokuapp.com/desserts")
-      .then((res) => res.json())
-      .then((data) => setDesserts(data));
-  }, []);
+function Desserts({desserts}) {
+
   return (
     <div className="background">
       <Container>
